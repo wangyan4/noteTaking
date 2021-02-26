acdc29e2759e921d3091daf1564e9075d62dd28d
wangyan25 <wangyan25@yonyou.com>
2021-02-26 17:08:00 +0800

    编辑删除预览

--- src/web/src/components/body/body.js
+++ src/web/src/components/body/body.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { Menu, Button } from 'antd';
+import React, { Component, createRef } from 'react';
+import { Menu, Button, Modal } from 'antd';
+import _ from 'loadsh';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -7,19 +8,58 @@ import {
   DesktopOutlined,
   ContainerOutlined
 } from '@ant-design/icons';
-import Editor from '../Editor/editor'
-import NoteList from '../note/note'
+import Editor from '../Editor/editor';
+import NoteList from '../note/note';
+
 
 import './body.css';
-// const { SubMenu } = Menu;
-
+function GenNonDuplicateID(randomLength) {
+  return Number(Math.random().toString().substr(3, randomLength) + Date.now()).toString(36)
+}
+const data = [
+  {
+    id: GenNonDuplicateID(8),
+    title: '你好，李焕英',
+    description: "这是一段描述",
+    content: "<p>你好，李焕英</p>"
+  },
+  {
+    id: GenNonDuplicateID(8),
+    title: '沐浴之王',
+    description: "这是一段描述",
+    content: "<p>沐浴之王</p>"
+  }
+];
+const data1 = [
+  {
+    id: GenNonDuplicateID(8),
+    title: '战狼2',
+    description: "这是一段描述",
+    content: "<p>战狼2</p>"
+  },
+  {
+    id: GenNonDuplicateID(8),
+    title: '功夫之王',
+    description: "这是一段描述",
+    content: "<p>功夫之王</p>"
+  }
+];
 
 export default class NavBar extends Component {
-  state = {
-    collapsed: false,
-    showPage:1
-  };
-
+  constructor() {
+    super();
+    this.preview = createRef();
+    this.state = {
+      collapsed: false,
+      showPage: 1,
+      isShow: true,
+      editItem: data[0],
+      data: data,
+      data1: data1,
+      flag: false,
+      modalVisible: false
+    };
+  }
   toggleCollapsed = () => {
     this.setState({
       collapsed: !this.state.collapsed,
@@ -27,17 +67,34 @@ export default class NavBar extends Component {
   };
 
   menuChange = (code) => {
-    var changeObj={
-      note:"1",
-      ground:"2",
-      my:"3"
-
+    var changeObj = {
+      note: "1",
+      ground: "2",
+      my: "3"
     }
-    // message.info(code, 2)
-    this.setState({showPage:changeObj[code]});
+    this.setState({ showPage: changeObj[code], isShow: true, flag:false });
   }
-  previewStatus = (status)=>{
-    console.log(status);
+  previewStatus = (editItem) => {
+    console.log(editItem);
+    this.setState({ editItem, isShow: editItem.flag, flag: editItem.preview }, () => {
+      console.log(this.state)
+    })
+    if (editItem.preview) {
+      this.preview.current.innerHTML = editItem.content;
+    }
+  }
+
+  setModalVisible(modalVisible) {
+    this.setState({ modalVisible });
+  }
+
+  delItem = (editItem) => {
+    this.setModalVisible(true);
+    this.setState({ editItem });
+  }
+
+  addItem = () => {
+
   }
 
   render() {
@@ -54,40 +111,63 @@ export default class NavBar extends Component {
             <Button type="primary" onClick={this.toggleCollapsed} style={this.state.collapsed ? { width: 80 } : { width: "100%" }}>
               {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
             </Button>
-            <Menu.Item key="1" icon={<PieChartOutlined />} onClick={this.menuChange.bind(this,'note')}>
+            <Menu.Item key="1" icon={<PieChartOutlined />} onClick={this.menuChange.bind(this, 'note')}>
               笔记
             </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />} onClick={this.menuChange.bind(this,'ground')}>
+            <Menu.Item key="2" icon={<DesktopOutlined />} onClick={this.menuChange.bind(this, 'ground')}>
               分享广场
             </Menu.Item>
-            <Menu.Item key="3" icon={<ContainerOutlined />} onClick={this.menuChange.bind(this,'my')}>
+            <Menu.Item key="3" icon={<ContainerOutlined />} onClick={this.menuChange.bind(this, 'my')}>
               我的
             </Menu.Item>
           </Menu>
         </div>
         <div className="content">
           {
-            this.state.showPage == 1 
-            ?<div className="noteList">
-              <NoteList preview={this.previewStatus.bind(this)} />
+            this.state.showPage == 1 && this.state.isShow
+              ? <div className="noteList">
+                <Button type="primary" shape="round" onClick={() => { this.setState({ isShow: false }) }}>新建</Button>
+                {/* <Button type="default">新增</Button> */}
+                <NoteList preview={this.previewStatus} data={this.state.data} delItem={this.delItem} />
               </div>
-            :""
+              : null
           }
           {
-            this.state.showPage == 1 
-            ?<div className="editor">
-              <Editor />
-            </div>
-            :""
+            this.state.showPage == 1
+              ? <div className="editor">
+                <Editor editItem={this.state.editItem} />
+              </div>
+              : null
           }
           {
-            this.state.showPage == 2 
-            ?<div className="noteList">
-              <NoteList preview={this.previewStatus.bind(this)} isPersonal/>
+            this.state.showPage == 2
+              ? <div className="noteList">
+                <NoteList preview={this.previewStatus} isPersonal data={this.state.data1} delItem={this.delItem} />
               </div>
-            :""
+              : null
+          }
+          {
+              <div className="editorPreview" ref={this.preview} style={!this.state.flag?{display:"none"}:{display:"block"}}>
+
+              </div>
           }
         </div>
+        <Modal
+          title="删除"
+          style={{ top: 20 }}
+          visible={this.state.modalVisible}
+          onOk={() => {
+            this.setModalVisible(false);
+            let _this = this;
+            let newList = _.filter(_this.state.data, function (o) {
+              return o.id != _this.state.editItem.id;
+            })
+            this.setState({ data: newList })
+          }}
+          onCancel={() => this.setModalVisible(false)}
+        >
+          <p>确认要删除吗?</p>
+        </Modal>
       </div>
     );
   }
--- src/web/src/components/note/note.js
+++ src/web/src/components/note/note.js
@@ -1,67 +1,71 @@
 import React, { Component } from 'react';
-import { List, Avatar, Skeleton } from 'antd';
+import { List, Avatar, message } from 'antd';
 
-function GenNonDuplicateID(randomLength){
-  return Number(Math.random().toString().substr(3,randomLength) + Date.now()).toString(36)
-}
-const data = [
-  {
-    id:GenNonDuplicateID(8),
-    title: 'Ant Design Title 1',
-  },
-  {
-    id:GenNonDuplicateID(8),
-    title: 'Ant Design Title 2',
-  }
-];
-
+// function GenNonDuplicateID(randomLength) {
+//   return Number(Math.random().toString().substr(3, randomLength) + Date.now()).toString(36)
+// }
 export default class NoteList extends Component {
   state = {
-
+    listData: []
   }
-
-  componentDidMount() {
-    console.log(this.props.preview)
+  // 在getDerivedStateFromProps中进行state的改变
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      listData: nextProps.data,
+    };
+  }
+  //编辑当前列表项
+  editList = (id, content, flag) => {
+    this.props.preview({ id, content, flag, preview: false })
+  }
+  //删除当前列表项
+  delList = (item) => {
+    this.props.delItem(item);
+  }
+  //广场预览分享数据
+  previewList = (item, flag) => {
+    item.preview = flag;
+    this.props.preview(item)
   }
 
   render() {
-    console.log(this)
+    let { listData } = this.state;
     return (
       <div>
         {
           this.props.isPersonal
-            ?<List
-            itemLayout="horizontal"
-            dataSource={data}
-            renderItem={item => (
-              <List.Item 
-                actions={[<a style={{marginRight:30}} key="list-loadmore-edit">预览</a>]}
-              >
-                <List.Item.Meta
-                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                  title={<a href="#" onClick={()=>{this.props.preview(item.id)}}>{item.title}</a>}
-                  description="这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述"
-                />
-              </List.Item>
-            )}
-          />
-            :<List
-            itemLayout="vertical"
-            dataSource={data}
-            renderItem={item => (
-              <List.Item 
-                actions={this.props.isPersonal?"":[<a style={{marginLeft:50}} key="list-loadmore-edit">编辑</a>, <a key="list-loadmore-delete">删除</a>]}
-              >
-                <List.Item.Meta
-                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                  title={<a href="#" onClick={()=>{this.props.preview(item.id)}}>{item.title}</a>}
-                  description="这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述这是一段很长很长的描述"
-                />
-              </List.Item>
-            )}
-          />
+            ? <List
+              itemLayout="horizontal"
+              dataSource={listData}
+              renderItem={item => (
+                <List.Item
+                  actions={[<a style={{ marginRight: 30 }} key="list-loadmore-edit" onClick={() => { this.previewList(item, true) }}>预览</a>]}
+                >
+                  <List.Item.Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title={<a onClick={() => { this.editList(item.id, item.content, true) }}>{item.title}</a>}
+                    description={item.description}
+                  />
+                </List.Item>
+              )}
+            />
+            : <List
+              itemLayout="vertical"
+              dataSource={listData}
+              renderItem={item => (
+                <List.Item
+                  actions={this.props.isPersonal ? "" : [<a style={{ marginLeft: 50 }} key="list-loadmore-edit" onClick={() => { this.editList(item.id, item.content, false) }}>编辑</a>, <a key="list-loadmore-delete" onClick={() => { this.delList(item) }}>删除</a>]}
+                >
+                  <List.Item.Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title={<a onClick={() => { this.editList(item.id, item.content, true) }}>{item.title}</a>}
+                    description={item.description}
+                  />
+                </List.Item>
+              )}
+            />
         }
-        
+
       </div>
     );
   }
